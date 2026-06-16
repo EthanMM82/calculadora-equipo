1d4314704e754a4715f89fe36a46f0dc6fd3971a
a26110123-spec <a26110123@ceti.mx>
2026-06-15 23:24:25 -0600

    Add files via upload

--- suma.c
+++ suma.c
@@ -1,17 +1,42 @@
 #include <stdio.h>
 
-int main() {
+int main(){
+    int menu;
     float num1, num2, resultado;
+    printf ("Ingrese un número.\n");
+    scanf (" %f", &num1);
+    printf ("Ingrese otro número.\n");
+    scanf (" %f", &num2);
+    
+    printf ("Calculadora en Switch.\n");
+    printf ("1. Suma\n");
+    printf ("2. Resta\n");
+    printf ("3. Multiplicación\n");
+    printf ("4. Division\n");
+    scanf("%d", &menu);
+    
+    switch (menu){
+        case 1:
+            //Ethan
+            resultado = (float) num1 + num2;
+        break;
+        case 2:
+            //Rodrigo
+            resultado = (float) num1 - num2;
+        break;
+        case 3:
+            //Martin
+            resultado = (float) num1 * num2;
+        break;
+        case 4:
+            //Juan José
+            resultado = (float) num1 / num2;
+        break;
+        default:
+            printf ("Opción Errónea");
+        break;
 
-    printf("--- Calculadora de Suma ---\n");
-    printf("Ingresa el primer numero: ");
-    scanf("%f", &num1);
-    printf("Ingresa el segundo numero: ");
-    scanf("%f", &num2);
-
-    resultado = num1 + num2;
-
-    printf("El resultado de la suma es: %.2f\n", resultado);
-
+    }
+     printf("El resultado es: %.2f", resultado); 
     return 0;
 }
